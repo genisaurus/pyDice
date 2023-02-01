dc5f209488a1d6cc16258615fdc426434471f37f
Genisaurus <13349928+genisaurus@users.noreply.github.com>
2023-01-31 21:50:39 -0500

    refactored to use regex matches instead of string manipulation based on ops character positions.

--- dice.py
+++ dice.py
@@ -1,8 +1,12 @@
 #! /usr/bin/python3
 
-import random, math
+import random, re
 
-ops = ['d','<','>','+','-']
+ops = {'die':'d', 'low':'<', 'high':'>', 'plus':'+', 'minus':'-'}
+regex_string = r'''^(?P<dice>\d+{0}\d+)
+        (?P<selects>([{1}{2}]\d+)*)
+        (?P<scalars>([{3}{4}]\d+)*)$'''.format(ops['die'], ops['low'], ops['high'], ops['plus'], ops['minus'])
+pattern = re.compile(regex_string, re.VERBOSE)
 
 def print_help():
     """
@@ -22,123 +26,85 @@ def print_help():
     print("5d6<4>3+2-1+6 is valid. 5d6<4>3+2-1>6 is not.\n")
 #######################################################################################################################
 
-def validate(dice_string, ops_char_pos):
+def validate(dice_string):
     """
     Checks whether the dice_string is a valid formula
 
     :param dice_string:     text reprepsenting the dice formula to be validated
-    :param ops_char_pos:    list of indices at which Ops characters appear in dice_string
     :returns:               a boolean value indicating whether dice_string meets formatting requirements
     """
-    # check if the NdS format is met
-    if len(dice_string)<3:
-        print("Error: dice string too short")
-        return False
-    if dice_string.find('d') == -1:
-        print("Error: NdS format not found")
+
+    if pattern.match(dice_string) == None:
+        print("Error: Invalid die formula")
         return False
 
-    # check that the first Ops character is a 'd'
-    if not ops_char_pos[0] == dice_string.find('d'):
-        print("Error: dice string does not begin with NdS format")
-        return False
-    # check that the first characters before the 'd' are numeric:
-    if not dice_string[:ops_char_pos[0]].isdigit():
-        print("Error: invalid number of dice")
-        return False
-    # check only digits exist after Ops characters
-    for i in range(1, len(ops_char_pos)-1):
-        substr = dice_string[(ops_char_pos[i]+1):ops_char_pos[i+1]]
-        if not substr.isdigit():
-            print("Error: Invalid character found in dice string, ", substr )
+    # Check if each successive selection mod is lower than the previous
+    selections = re.search(pattern, dice_string).group('selects')
+    # Get an array of selection counts, then make sure they descend
+    vals = [int(i) for i in selections.replace('>','<').split('<')[1:]]
+    for i in range(1, len(vals)-1):
+        if vals[i] > vals[i-1]:
+            print("Error: you cannot select more dice than you have (", selections, ")")
             return False
-    # check that the final characters are digits
-    if not dice_string[(ops_char_pos[-1]+1):].isdigit():
-        print("Error: final segment not a valid number, ", dice_string[ops_char_pos[-1]:])
-        return False
+
     return True
 #######################################################################################################################
 
-def roll(dice_string, ops_char_pos):
+def roll(dice_string):
     """
     Isolates the die count and sides, then generates the appropriate rolls
 
     :param dice_string:     text reprepsenting the dice formula to be validated
-    :param ops_char_pos:    list of indices at which Ops characters appear in dice_string
     :returns:               a sorted, size-N list of randomly generated integers between 1...S
     """
-    num = int(dice_string[:ops_char_pos[0]])
-    sides = 0
-    if len(ops_char_pos) > 1:
-        sides = int(dice_string[(ops_char_pos[0]+1):ops_char_pos[1]])
-    else:
-        sides = int(dice_string[(ops_char_pos[0]+1):])
+    dice = re.search(pattern, dice_string).group('dice')
+    num = int(dice.split(ops['die'])[0])
+    sides = int(dice.split(ops['die'])[1])
+
     rolls = []
     for i in range(num):
         rolls.append(random.randint(1,sides))
-
-    # remove the 'd' Op from the list
-    del ops_char_pos[0]
-
     rolls.sort()
+
     return rolls
 #######################################################################################################################
 
-def selection_mods(rolls, dice_string, ops_char_pos):
+def selection_mods(rolls, dice_string):
     """
     Adjusts the die rolls by keeping a series of the highest or lowest values
 
     :rolls:                 a sorted, size-N list of randomly generated integers between 1...S
     :param dice_string:     text reprepsenting the dice formula to be validated
-    :param ops_char_pos:    list of indices at which Ops characters appear in dice_string
     :returns:               a sorted and pruned size-N list of randomly generated integers between 1...S
     """
-    # while there are selection ops left to perform...
-    while len(ops_char_pos) > 0 and dice_string[ops_char_pos[0]] in ops[1:3]:
-        selection_count = 0
-        # split behavior depending on whether the current op is the last in the string
-        if len(ops_char_pos) == 1:
-            selection_count = int(dice_string[(ops_char_pos[0]+1):])
-        else:
-            selection_count = int(dice_string[(ops_char_pos[0]+1):ops_char_pos[1]])
-        # select the highest n or lowest n rolls
-        if dice_string[ops_char_pos[0]] == '<':
-            rolls = rolls[:selection_count]
-        elif dice_string[ops_char_pos[0]] == '>':
-            rolls = rolls[(-1 * selection_count):]
-        del ops_char_pos[0]
-
+    selections = re.search(pattern, dice_string).group('selects')
+    mods = re.findall(r'([{0}{1}]\d+)'.format(ops['low'], ops['high']), selections)
+    # ['>40', '<3', ...]
+    for mod in mods:
+        if mod[0] == ops['low']:
+            rolls = rolls[0:int(mod[1:])]
+        elif mod[0] == ops['high']:
+            rolls = rolls[(-1 * int(mod[1:])):]
+    
     return rolls
 #######################################################################################################################
 
-def scalar_mods(rolls, dice_string, ops_char_pos):
+def scalar_mods(rolls, dice_string):
     """
     Sums the list of die rolls and then adds/subtracts a series of scalar adjustments
 
     :rolls:                 a sorted, size-N list of randomly generated integers between 1...S
     :param dice_string:     text reprepsenting the dice formula to be validated
-    :param ops_char_pos:    list of indices at which Ops characters appear in dice_string
     :returns:               An integer which is the sum of the list of die rolls and any scalar adjustments
     """
+    scalars = re.search(pattern, dice_string).group('scalars')
+    mods = re.findall(r'([{0}{1}]\d+)'.format(ops['plus'], ops['minus']), scalars)    
 
     final = 0
     for r in rolls:
         final += r
-
-    # while there are scalar ops left to perform...
-    while len(ops_char_pos) > 0 and dice_string[ops_char_pos[0]] in ops[3:5]:
-        value = 0
-        # split behavior depending on whether the current op is the last in the string
-        if len(ops_char_pos) == 1:
-            value = int(dice_string[(ops_char_pos[0]+1):])
-        else:
-            value = int(dice_string[(ops_char_pos[0]+1):ops_char_pos[1]])
-
-        if dice_string[ops_char_pos[0]] == '+':
-            final += value
-        elif dice_string[ops_char_pos[0]] == '-':
-            final -= value
-        del ops_char_pos[0]
+    for mod in mods:
+        final += int(mod)
 
     return final
 #######################################################################################################################
@@ -158,19 +124,15 @@ def main():
             print_help()
             continue;
         
-        # build a list of Ops character locations for parsing
-        for i in range(len(dice_string)):
-            if dice_string[i] in ops:
-                ops_char_pos.append(i)
-        valid_string = validate(dice_string, ops_char_pos)
+        valid_string = validate(dice_string)
         if not valid_string:
             break;
 
-        rolls = roll(dice_string, ops_char_pos)
+        rolls = roll(dice_string)
         print("You rolled: ", rolls)
-        rolls = selection_mods(rolls, dice_string, ops_char_pos)
+        rolls = selection_mods(rolls, dice_string)
         print("After Selections: ", rolls)
-        final = scalar_mods(rolls, dice_string, ops_char_pos)
+        final = scalar_mods(rolls, dice_string)
         print("Your final roll: ", final)
 #######################################################################################################################
 
